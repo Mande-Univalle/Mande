9c9b887f62ef85330763a7f7d116b640076e0f2f
sebastian6551 <sebastian6551@live.com>
2022-08-08 18:49:41 -0500

    DB schema

--- database_postgres/schema_v1.sql
+++ database_postgres/schema_v1.sql
@@ -14,9 +14,55 @@ CREATE DATABASE mande_db
 
 \c mande_db
 
+CREATE TABLE medio_pago(
+	id_medio SERIAL PRIMARY KEY,
+	tipo_pago VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE labor(
+	id_labor SERIAL PRIMARY KEY,
+	tipo_labor VARCHAR(50) UNIQUE
+);
+
 CREATE TABLE usuario(
-	id_usuario SERIAL PRIMARY KEY,
-	nombre_usuario VARCHAR(60) UNIQUE,
-	password VARCHAR(60) CHECK (length(password) > 3),
-	premium BOOLEAN DEFAULT FALSE
+	telefono_usuario VARCHAR(20) PRIMARY KEY,
+	nombre_usuario VARCHAR(50),
+    apellido_usuario VARCHAR(50),
+    medio_pago INTEGER REFERENCES medio_pago,
+    cc_usuario VARCHAR(20),
+    email VARCHAR(100),
+    direccion_usuario VARCHAR(60),
+    recibo_evidencia BYTEA,
+    numero_tarjeta  VARCHAR(60)
+);
+
+
+CREATE TABLE trabajador(
+	cc_trabajador VARCHAR(50) PRIMARY KEY,
+	nombre_trabajador VARCHAR(50),
+	apellido_trabajador VARCHAR(50),
+	estrellas FLOAT,
+	estado BOOLEAN DEFAULT(false),
+	direccion_trabajador VARCHAR(200),
+	telefono_trabajador VARCHAR(20),
+	foto_cc_trabajador BYTEA,
+	foto_perfil_trabajador BYTEA
+);
+
+CREATE TABLE servicio(
+    id_servicio SERIAL PRIMARY KEY,
+    telefono_usuario VARCHAR(20) REFERENCES usuario,
+    cc_trabajador VARCHAR(16) REFERENCES trabajador,
+    id_labor INTEGER REFERENCES labor,
+    fecha_servicio DATE,
+    costo_servicio FLOAT,
+    calificacion_servicio INTEGER
+);
+
+CREATE TABLE desempegna(
+    cc_trabajador VARCHAR(16) REFERENCES trabajador,
+    id_labor INTEGER REFERENCES labor,
+    precio_labor FLOAT,
+    descripcion_labor VARCHAR(50),
+    PRIMARY KEY (cc_trabajador,id_labor)
 );
